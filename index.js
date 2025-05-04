ed4232ccee85b245e2b27c5fd08874526f2337c0
Salim31 <salimalhaddad1211@gmail.com>
2025-05-04 18:58:44 +0700

    TUTAM9 - Perbaikan request

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 
